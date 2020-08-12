dd6285c3fd52116f6fce0307cb77ac1deb4bc9eb
tojatos <tojatos@gmail.com>
2020-08-12 18:44:55 +0200

    Refactor of future nesting

--- src/main/scala/com/tosware/NKM/Main.scala
+++ src/main/scala/com/tosware/NKM/Main.scala
@@ -9,7 +9,7 @@ import akka.util.Timeout
 import com.tosware.NKM.actors.Game._
 import com.tosware.NKM.actors.NKMData.GetHexMaps
 import com.tosware.NKM.actors._
-import com.tosware.NKM.models.{GameState, HexCoordinates, HexMap, NKMCharacter, Stat}
+import com.tosware.NKM.models.{GameState, HexMap}
 import com.tosware.NKM.serializers.NKMJsonProtocol
 
 import scala.concurrent.Await
@@ -17,30 +17,24 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 
 object Main extends App with NKMJsonProtocol with SprayJsonSupport {
-  import system.dispatcher
-
   implicit val system: ActorSystem = ActorSystem("NKMServer")
 
   val nkmData = system.actorOf(NKMData.props())
   implicit val timeout: Timeout = Timeout(2 seconds)
-  val hexMaps = (nkmData ? GetHexMaps).mapTo[List[HexMap]]
-  val oneHexMap = hexMaps.map(maps => maps.head)
-  for {
-    hexMaps <- (nkmData ? GetHexMaps).mapTo[List[HexMap]]
-  }{
-    val game = system.actorOf(Game.props("1", hexMaps.head))
-    val skeleton =
-      pathPrefix("api") {
-        get {
-          path("state") {
-            complete((game ? GetState).mapTo[GameState])
-          } ~
-          path("maps") {
-            complete((nkmData ? GetHexMaps).mapTo[List[HexMap]])
-          }
+  val hexMaps = Await.result((nkmData ? GetHexMaps).mapTo[List[HexMap]], 2 seconds)
+  val game = system.actorOf(Game.props("1", hexMaps.head))
+  val skeleton =
+    pathPrefix("api") {
+      get {
+        path("state") {
+          complete((game ? GetState).mapTo[GameState])
+        } ~
+        path("maps") {
+          complete((nkmData ? GetHexMaps).mapTo[List[HexMap]])
         }
       }
-    Http().newServerAt("localhost", 8080).bindFlow(skeleton)
+    }
+  Http().newServerAt("localhost", 8080).bindFlow(skeleton)
 
 //    val playerNames = List("Ryszard", "Ania", "Ola")
 //    val characters: List[NKMCharacter] = List[NKMCharacter](
@@ -58,5 +52,4 @@ object Main extends App with NKMJsonProtocol with SprayJsonSupport {
 //
 //    game ! PlaceCharacter(HexCoordinates(4, 5), touka)
 //    game ! MoveCharacter(HexCoordinates(0, 0), touka)
-  }
 }
