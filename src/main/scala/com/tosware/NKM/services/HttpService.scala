17e33acea05cf8c5e447a3dbc15a63cf5b489c3f
tojatos <tojatos@gmail.com>
2022-02-03 15:27:06 +0100

    logging http requests, responses and rejections added

--- src/main/scala/com/tosware/NKM/services/HttpService.scala
+++ src/main/scala/com/tosware/NKM/services/HttpService.scala
@@ -1,10 +1,13 @@
 package com.tosware.NKM.services
 
 import akka.actor.{ActorRef, ActorSystem}
+import akka.event.Logging.LogLevel
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
 import akka.http.scaladsl.server.Directives.{entity, _}
-import akka.http.scaladsl.server.{Directive1, Route}
+import akka.http.scaladsl.server.directives.{LogEntry, LoggingMagnet}
+import akka.http.scaladsl.server.{Directive1, Route, RouteResult}
 import com.tosware.NKM.actors.User.{RegisterFailure, RegisterSuccess}
 import com.tosware.NKM.actors._
 import com.tosware.NKM.models._
@@ -21,7 +24,7 @@ import scala.language.postfixOps
 import scala.util.{Failure, Success}
 
 trait HttpService
-    extends CORSHandler
+  extends CORSHandler
     with SprayJsonSupport
     with NKMJsonProtocol
     with NKMTimeouts
@@ -56,166 +59,202 @@ trait HttpService
     token
   }
 
-  val routes: Route =
-    corsHandler {
-      pathPrefix("api") {
-        get {
-          concat(
-            path("state"/ Segment) { (gameId: String) =>
-              complete(gameService.getGameState(gameId))
-            },
-            path("maps") {
-              complete(NKMDataService.getHexMaps)
-            },
-            path("characters") {
-              complete(NKMDataService.getCharactersMetadata)
-            },
-            path("secret") {
-              authenticated { jwtClaim =>
-                complete(jwtClaim.content)
-              }
-            },
-            path("lobbies") {
-              val lobbies = lobbyService.getAllLobbies()
-              complete(lobbies)
-            },
-            path("lobby" / Segment) { (lobbyId: String) =>
-              val lobby = lobbyService.getLobby(lobbyId)
-              complete(lobby)
-            },
-          )
-        } ~
-        post {
-          concat(
-            path("register") {
-              entity(as[RegisterRequest]) { entity =>
-                println(s"Received register request for ${entity.login}")
-                userService.register(entity) match {
-                  case RegisterSuccess => complete(StatusCodes.Created)
-                  case RegisterFailure => complete(StatusCodes.Conflict) // TODO - change status code based on failure
-                }
-              }
-            },
-            path("login") {
-              entity(as[Credentials]) { entity =>
-                println(s"Logging in ${entity.login}")
-                userService.authenticate(entity) match {
-                  case LoggedIn(login) => complete(StatusCodes.OK, getToken(login))
-                  case InvalidCredentials => complete(StatusCodes.Unauthorized, "invalid credentials")
-                }
-              }
-            },
-            path("create_lobby") {
-              authenticated { jwtClaim =>
-                entity(as[LobbyCreationRequest]) { entity =>
-                  import LobbyService._
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.createLobby(entity.name, username) match {
-                    case LobbyCreated(lobbyId) => complete(StatusCodes.Created, lobbyId)
-                    case LobbyCreationFailure => complete(StatusCodes.InternalServerError)
-                  }
-                }
-              }
-            },
+  def akkaResponseTimeLoggingFunction(
+                                       loggingAdapter: LoggingAdapter,
+                                       requestTimestamp: Long,
+                                       level: LogLevel = Logging.InfoLevel)(req: HttpRequest)(res: RouteResult): Unit = {
+    val entry = res match {
+      case RouteResult.Complete(res) =>
+        val responseTimestamp = System.nanoTime
+        val elapsedTime = (responseTimestamp - requestTimestamp) / 1000000
+        val loggingString =
+          s"""
+             |${req.uri}
+             |${req.method.name} | ${res.status} | ${res.entity.contentType.toString}
+             |Elapsed time: $elapsedTime ms""".stripMargin
+        LogEntry(loggingString, level)
 
-            path("join_lobby") {
-              authenticated { jwtClaim =>
-                entity(as[LobbyJoinRequest]) { entity =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.joinLobby(username, entity) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
-                  }
-                }
-              }
-            },
+      case RouteResult.Rejected(reason) =>
+        val loggingString =
+          s"""
+             |${req.uri}
+             |${req.method.name}
+             |Rejected. ${reason.mkString(",")}""".stripMargin
+        LogEntry(loggingString, level)
+    }
+    entry.logTo(loggingAdapter)
+  }
 
-            path("leave_lobby") {
-              authenticated { jwtClaim =>
-                entity(as[LobbyLeaveRequest]) { entity =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.leaveLobby(username, entity) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
-                  }
-                }
-              }
-            },
+  def printResponseTime(log: LoggingAdapter) = {
+    val requestTimestamp = System.nanoTime
+    akkaResponseTimeLoggingFunction(log, requestTimestamp) _
+  }
 
-            path("set_hexmap") {
-              authenticated { jwtClaim =>
-                entity(as[SetHexMapNameRequest]) { entity =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.setHexmapName(username, entity) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
-                  }
-                }
-              }
-            },
+  val logResponseTime = logRequestResult(LoggingMagnet(printResponseTime))
 
-            path("set_pick_type") {
-              authenticated { jwtClaim =>
-                entity(as[SetPickTypeRequest]) { request =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.setPickType(username, request) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
-                  }
+  val routes: Route = {
+    logResponseTime {
+      corsHandler {
+        pathPrefix("api") {
+          get {
+            concat(
+              path("state"/ Segment) { (gameId: String) =>
+                complete(gameService.getGameState(gameId))
+              },
+              path("maps") {
+                complete(NKMDataService.getHexMaps)
+              },
+              path("characters") {
+                complete(NKMDataService.getCharactersMetadata)
+              },
+              path("secret") {
+                authenticated { jwtClaim =>
+                  complete(jwtClaim.content)
                 }
-              }
-            },
+              },
+              path("lobbies") {
+                val lobbies = lobbyService.getAllLobbies()
+                complete(lobbies)
+              },
+              path("lobby" / Segment) { (lobbyId: String) =>
+                val lobby = lobbyService.getLobby(lobbyId)
+                complete(lobby)
+              },
+            )
+          } ~
+            post {
+              concat(
+                path("register") {
+                  entity(as[RegisterRequest]) { entity =>
+                    println(s"Received register request for ${entity.login}")
+                    userService.register(entity) match {
+                      case RegisterSuccess => complete(StatusCodes.Created)
+                      case RegisterFailure => complete(StatusCodes.Conflict) // TODO - change status code based on failure
+                    }
+                  }
+                },
+                path("login") {
+                  entity(as[Credentials]) { entity =>
+                    println(s"Logging in ${entity.login}")
+                    userService.authenticate(entity) match {
+                      case LoggedIn(login) => complete(StatusCodes.OK, getToken(login))
+                      case InvalidCredentials => complete(StatusCodes.Unauthorized, "invalid credentials")
+                    }
+                  }
+                },
+                path("create_lobby") {
+                  authenticated { jwtClaim =>
+                    entity(as[LobbyCreationRequest]) { entity =>
+                      import LobbyService._
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.createLobby(entity.name, username) match {
+                        case LobbyCreated(lobbyId) => complete(StatusCodes.Created, lobbyId)
+                        case LobbyCreationFailure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
+                  }
+                },
 
-            path("set_number_of_bans") {
-              authenticated { jwtClaim =>
-                entity(as[SetNumberOfBansRequest]) { request =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.setNumberOfBans(username, request) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                path("join_lobby") {
+                  authenticated { jwtClaim =>
+                    entity(as[LobbyJoinRequest]) { entity =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.joinLobby(username, entity) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
                   }
-                }
-              }
-            },
+                },
 
-            path("set_number_of_characters") {
-              authenticated { jwtClaim =>
-                entity(as[SetNumberOfCharactersPerPlayerRequest]) { request =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.setNumberOfCharactersPerPlayer(username, request) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                path("leave_lobby") {
+                  authenticated { jwtClaim =>
+                    entity(as[LobbyLeaveRequest]) { entity =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.leaveLobby(username, entity) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
                   }
-                }
-              }
-            },
+                },
 
-            path("start_game") {
-              authenticated { jwtClaim =>
-                entity(as[StartGameRequest]) { entity =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  lobbyService.startGame(username, entity) match {
-                    case LobbyService.Success => complete(StatusCodes.OK)
-                    case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                path("set_hexmap") {
+                  authenticated { jwtClaim =>
+                    entity(as[SetHexMapNameRequest]) { entity =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.setHexmapName(username, entity) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
                   }
-                }
-              }
-            },
+                },
 
-            path("place_character") {
-              authenticated { jwtClaim =>
-                entity(as[PlaceCharacterRequest]) { entity =>
-                  val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
-                  onSuccess(gameService.placeCharacter(username, entity)) {
-                    case CommandResponse.Success => complete(StatusCodes.OK)
-                    case CommandResponse.Failure => complete(StatusCodes.InternalServerError)
+                path("set_pick_type") {
+                  authenticated { jwtClaim =>
+                    entity(as[SetPickTypeRequest]) { request =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.setPickType(username, request) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
                   }
-                }
-              }
-            },
-          )
+                },
+
+                path("set_number_of_bans") {
+                  authenticated { jwtClaim =>
+                    entity(as[SetNumberOfBansRequest]) { request =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.setNumberOfBans(username, request) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
+                  }
+                },
+
+                path("set_number_of_characters") {
+                  authenticated { jwtClaim =>
+                    entity(as[SetNumberOfCharactersPerPlayerRequest]) { request =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.setNumberOfCharactersPerPlayer(username, request) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
+                  }
+                },
+
+                path("start_game") {
+                  authenticated { jwtClaim =>
+                    entity(as[StartGameRequest]) { entity =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      lobbyService.startGame(username, entity) match {
+                        case LobbyService.Success => complete(StatusCodes.OK)
+                        case LobbyService.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
+                  }
+                },
+
+                path("place_character") {
+                  authenticated { jwtClaim =>
+                    entity(as[PlaceCharacterRequest]) { entity =>
+                      val username = jwtClaim.content.parseJson.convertTo[JwtContent].content
+                      onSuccess(gameService.placeCharacter(username, entity)) {
+                        case CommandResponse.Success => complete(StatusCodes.OK)
+                        case CommandResponse.Failure => complete(StatusCodes.InternalServerError)
+                      }
+                    }
+                  }
+                },
+              )
+            }
         }
       }
     }
+  }
 
 }
