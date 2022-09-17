b6394c0705ccc5f4b4771619cba405db5cb5b6a2
tojatos <tojatos@gmail.com>
2022-09-17 22:10:51 +0200

    added missing abilities to ability provider

--- src/main/scala/com/tosware/nkm/providers/AbilityProvider.scala
+++ src/main/scala/com/tosware/nkm/providers/AbilityProvider.scala
@@ -5,12 +5,16 @@ import com.tosware.nkm.models.game.Ability.AbilityMetadataId
 import com.tosware.nkm.models.game.NkmCharacter.CharacterId
 import com.tosware.nkm.models.game.abilities._
 import com.tosware.nkm.models.game.hex.NkmUtils
+import akame._
 import aqua._
 import blank._
+import carmel_wilhelmina._
 import crona._
 import hecate._
+import kirito._
 import llenn._
 import roronoa_zoro._
+import ryuko_matoi._
 import sinon._
 
 import scala.util.Random
@@ -18,19 +22,19 @@ import scala.util.Random
 object AbilityProvider {
   def instantiateAbilities(characterId: CharacterId, metadataIds: Seq[AbilityMetadataId])(implicit random: Random): Seq[Ability] = {
     metadataIds.map {
+      case Murasame.metadata.id =>
+        Murasame(NkmUtils.randomUUID(), characterId)
+      case Eliminate.metadata.id =>
+        Eliminate(NkmUtils.randomUUID(), characterId)
+      case LittleWarHorn.metadata.id =>
+        LittleWarHorn(NkmUtils.randomUUID(), characterId)
+
       case NaturesBeauty.metadata.id =>
         NaturesBeauty(NkmUtils.randomUUID(), characterId)
       case Purification.metadata.id =>
         Purification(NkmUtils.randomUUID(), characterId)
       case Resurrection.metadata.id =>
         Resurrection(NkmUtils.randomUUID(), characterId)
-
-      case BlackBlood.metadata.id =>
-        BlackBlood(NkmUtils.randomUUID(), characterId)
-      case ScreechAlpha.metadata.id =>
-        ScreechAlpha(NkmUtils.randomUUID(), characterId)
-      case Infection.metadata.id =>
-        Infection(NkmUtils.randomUUID(), characterId)
 
       case AceInTheHole.metadata.id =>
         AceInTheHole(NkmUtils.randomUUID(), characterId)
@@ -39,6 +43,20 @@ object AbilityProvider {
       case Castling.metadata.id =>
         Castling(NkmUtils.randomUUID(), characterId)
 
+      case ManipulatorOfObjects.metadata.id =>
+        ManipulatorOfObjects(NkmUtils.randomUUID(), characterId)
+      case BindingRibbons.metadata.id =>
+        BindingRibbons(NkmUtils.randomUUID(), characterId)
+      case TiamatsIntervention.metadata.id =>
+        TiamatsIntervention(NkmUtils.randomUUID(), characterId)
+
+      case BlackBlood.metadata.id =>
+        BlackBlood(NkmUtils.randomUUID(), characterId)
+      case ScreechAlpha.metadata.id =>
+        ScreechAlpha(NkmUtils.randomUUID(), characterId)
+      case Infection.metadata.id =>
+        Infection(NkmUtils.randomUUID(), characterId)
+
       case MasterThrone.metadata.id =>
         MasterThrone(NkmUtils.randomUUID(), characterId)
       case Aster.metadata.id =>
@@ -46,12 +64,12 @@ object AbilityProvider {
       case PowerOfExistence.metadata.id =>
         PowerOfExistence(NkmUtils.randomUUID(), characterId)
 
-      case SnipersSight.metadata.id =>
-        SnipersSight(NkmUtils.randomUUID(), characterId)
-      case TacticalEscape.metadata.id =>
-        TacticalEscape(NkmUtils.randomUUID(), characterId)
-      case PreciseShot.metadata.id =>
-        PreciseShot(NkmUtils.randomUUID(), characterId)
+      case Parry.metadata.id =>
+        Parry(NkmUtils.randomUUID(), characterId)
+      case Switch.metadata.id =>
+        Switch(NkmUtils.randomUUID(), characterId)
+      case StarburstStream.metadata.id =>
+        StarburstStream(NkmUtils.randomUUID(), characterId)
 
       case PChan.metadata.id =>
         PChan(NkmUtils.randomUUID(), characterId)
@@ -66,6 +84,20 @@ object AbilityProvider {
         OgreCutter(NkmUtils.randomUUID(), characterId)
       case OneHundredEightPoundPhoenix.metadata.id =>
         OneHundredEightPoundPhoenix(NkmUtils.randomUUID(), characterId)
+
+      case ScissorBlade.metadata.id =>
+        ScissorBlade(NkmUtils.randomUUID(), characterId)
+      case FiberDecapitation.metadata.id =>
+        FiberDecapitation(NkmUtils.randomUUID(), characterId)
+      case GodrobeSenketsu.metadata.id =>
+        GodrobeSenketsu(NkmUtils.randomUUID(), characterId)
+
+      case SnipersSight.metadata.id =>
+        SnipersSight(NkmUtils.randomUUID(), characterId)
+      case TacticalEscape.metadata.id =>
+        TacticalEscape(NkmUtils.randomUUID(), characterId)
+      case PreciseShot.metadata.id =>
+        PreciseShot(NkmUtils.randomUUID(), characterId)
     }
   }
 }
