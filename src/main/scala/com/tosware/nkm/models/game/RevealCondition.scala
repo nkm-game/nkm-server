060091921af9a922e7d54a01b4e90bb6250e847a
tojatos <tojatos@gmail.com>
2023-02-16 08:13:18 +0100

    related trap revealed quickfix

--- src/main/scala/com/tosware/nkm/models/game/RevealCondition.scala
+++ src/main/scala/com/tosware/nkm/models/game/RevealCondition.scala
@@ -1,6 +1,5 @@
 package com.tosware.nkm.models.game
 
-import com.tosware.nkm.models.game.CharacterEffect.CharacterEffectId
 import com.tosware.nkm.models.game.NkmCharacter.CharacterId
 
 sealed trait RevealCondition
@@ -9,6 +8,6 @@ object RevealCondition {
   final case object CharacterPlacingFinished extends RevealCondition
   final case object BlindPickFinished extends RevealCondition
   final case class RelatedCharacterRevealed(characterId: CharacterId) extends RevealCondition
-  final case class RelatedTrapRevealed(effectId: CharacterEffectId) extends RevealCondition
+  final case class RelatedTrapRevealed(effectId: String) extends RevealCondition
 }
 
