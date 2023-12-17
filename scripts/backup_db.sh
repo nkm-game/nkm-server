0e43279d36441016f898ce5ac4e8b43c7af94111
tojatos <tojatos@gmail.com>
2023-12-17 20:55:04 +0100

    NKM-267 Add script for db backup and upload to s3

---
+++ scripts/backup_db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DB_CONTAINER_NAME=nkmactorserver-db-next-1
+DB_NAME=nkm_db
+DB_USER=root
+DB_PASSWORD=nkm_db_pass
+
+S3_BUCKET_NAME=nkm-game-bucket
+
+BACKUP_FILENAME="backup_$(date +'%Y%m%d_%H%M%S').sql"
+
+docker exec $DB_CONTAINER_NAME /usr/bin/mysqldump -u $DB_USER --password=$DB_PASSWORD $DB_NAME > $BACKUP_FILENAME
+
+aws s3 cp $BACKUP_FILENAME s3://$S3_BUCKET_NAME/$BACKUP_FILENAME
+
+rm $BACKUP_FILENAME
+
