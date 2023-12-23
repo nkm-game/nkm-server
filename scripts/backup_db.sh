58685647c45b24fdc90f2e5283bb0c9c30211837
tojatos <tojatos@gmail.com>
2023-12-23 17:55:22 +0100

    Use full aws path in backup db script to make it work with ssh

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -11,7 +11,7 @@ BACKUP_FILENAME="backup_$(date +'%Y%m%d_%H%M%S').sql"
 
 docker exec $DB_CONTAINER_NAME /usr/bin/mysqldump -u $DB_USER --password=$DB_PASSWORD $DB_NAME > $BACKUP_FILENAME
 
-aws s3 cp $BACKUP_FILENAME s3://$S3_BUCKET_NAME/$BACKUP_FILENAME
+/root/.local/bin/aws s3 cp $BACKUP_FILENAME s3://$S3_BUCKET_NAME/$BACKUP_FILENAME
 
 rm $BACKUP_FILENAME
 
