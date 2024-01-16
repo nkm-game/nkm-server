f61502e8beb40afc28accca361a4f09d4e6e2098
tojatos <tojatos@gmail.com>
2024-01-16 23:54:05 +0100

    logs testing

--- src/main/scala/com/tosware/nkm/Logging.scala
+++ src/main/scala/com/tosware/nkm/Logging.scala
@@ -1,7 +1,19 @@
 package com.tosware.nkm
 
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.{Logger, LoggerFactory, MDC}
 
 trait Logging {
   val logger: Logger = LoggerFactory.getLogger(getClass)
+}
+
+object Logging {
+  def withGameContext[A](gameId: String, logCategory: String)(block: => A): A = {
+    MDC.put("gameId", gameId)
+    MDC.put("gameType", logCategory)
+    try block
+    finally {
+      MDC.remove("gameId")
+      MDC.remove("gameType")
+    }
+  }
 }
