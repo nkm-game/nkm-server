339a2d6e45b47879eaa298fd90b5b74dbbc89621
tojatos <tojatos@gmail.com>
2024-06-27 00:35:35 +0200

    NKM-190 Add giving points at the end of a phase

--- src/main/scala/com/tosware/nkm/models/game/hex/TestHexMap.scala
+++ src/main/scala/com/tosware/nkm/models/game/hex/TestHexMap.scala
@@ -2,6 +2,10 @@ package com.tosware.nkm.models.game.hex
 
 import com.tosware.nkm.*
 
-case class TestHexMap(name: TestHexMapName, cellParams: Set[Any], pointParams: Seq[Set[(Int, Int)]] = Seq.empty) {
+case class TestHexMap(
+    name: TestHexMapName,
+    cellParams: Set[Any],
+    pointParams: Seq[(Set[(Int, Int)], Int)] = Seq.empty,
+) {
   val hexMap: HexMap = HexMap(name.toString, hexCellParamsToCells(cellParams), pointParamsToPointGroups(pointParams))
 }
